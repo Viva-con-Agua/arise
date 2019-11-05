f897569f72d14098b46c49233e772624c0a3df9b
Dennis Kleber <dennis_kleber@mailbox.org>
2019-11-05 08:38:45 +0100

    navigation update

--- release-docker.sh
+++ release-docker.sh
@@ -3,6 +3,7 @@ set -ex
 USERNAME=vivaconagua
 
 publish_vca_docker(){
+  docker tag $USERNAME/${1}:latest $USERNAME/${1}:${2};
   docker push $USERNAME/${1}:latest;
   docker push $USERNAME/${1}:${2};
 }
