e4143274be2f701b8bcebf0480c50e76b241bd4b
Tobias Kaestle <t.kaestle@vivaconagua.org>
2019-12-18 18:11:42 +0100

    updated active and nvm, changed format of birthday

--- src/components/ActiveFlag.vue
+++ src/components/ActiveFlag.vue
@@ -1,33 +1,40 @@
+<!--
+Component Active flag shows a button to 
+    * request active state, if the requirements therefore are fullfilled, this means the supporter has selected a crew (hasCrew)
+      This request has to be aproved by an ASP of the crew or an employee
+    * end the request, if the supporter has applied to become an active member of the crew
+    * end the active membership, if the request has been aproved
+-->
 <template>
-    	<div class="active-flag">
-		<div class="activeFlagTitle"><strong class="activeFlagTitle-title">{{ $t("profile.activeFlag.title") }}</strong> 
-			<span class="activeFlag-active" v-if="isActive">{{ $t("profile.activeFlag.current.active") }}</span>
-			<span class="activeFlag-requested" v-if="isRequested">{{ $t("profile.activeFlag.current.requested") }}</span>
-			<span class="activeFlag-inactive" v-if="canApply()">{{ $t("profile.activeFlag.current.inactive") }}</span>
-			<span class="activeFlag-denied" v-if="isDenied()">{{ $t("profile.activeFlag.current.denied") }}</span>
-		</div>
-		
-		<div class="active-flag-status" v-if="isActive">
-		  <span>{{ $t("profile.activeFlag.status.active") }}</span>
-		  <a class="vca-button-warn vca-full-width" style="cursor: pointer;" @click.prevent="handleSetInactiveRequest">{{ $t("profile.activeFlag.actions.active") }}</a>
-		</div>
+            <div class="active-flag">
+                <div class="activeFlagTitle"><strong class="activeFlagTitle-title">{{ $t("profile.activeFlag.title") }}</strong> 
+                        <span class="activeFlag-active" v-if="isActive">{{ $t("profile.activeFlag.current.active") }}</span>
+                        <span class="activeFlag-requested" v-if="isRequested">{{ $t("profile.activeFlag.current.requested") }}</span>
+                        <span class="activeFlag-inactive" v-if="canApply()">{{ $t("profile.activeFlag.current.inactive") }}</span>
+                        <span class="activeFlag-denied" v-if="isDenied()">{{ $t("profile.activeFlag.current.denied") }}</span>
+                </div>
 
-		<div class="active-flag-status" v-if="isRequested">
-		  <span>{{ $t("profile.activeFlag.status.requested") }}</span>
-		  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleUnsetActiveRequest">{{ $t("profile.activeFlag.actions.requested") }}</a>
-		</div>
+                <div class="active-flag-status" v-if="isActive">
+                  <span>{{ $t("profile.activeFlag.status.active") }}</span>
+                  <a class="vca-button-warn vca-full-width" style="cursor: pointer;" @click.prevent="handleSetInactiveRequest">{{ $t("profile.activeFlag.actions.active") }}</a>
+                </div>
 
-		<div class="active-flag-status" v-if="canApply()">
-		  <span>{{ $t("profile.activeFlag.status.apply") }}</span>
-		  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleActiveRequest">{{ $t("profile.activeFlag.actions.apply") }}</a>
-		</div>
+                <div class="active-flag-status" v-if="isRequested">
+                  <span>{{ $t("profile.activeFlag.status.requested") }}</span>
+                  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleUnsetActiveRequest">{{ $t("profile.activeFlag.actions.requested") }}</a>
+                </div>
 
-		<div class="active-flag-status" v-if="isDenied()">
-		  <span><strong>{{ $t("profile.warning.important") }}</strong></span>
-		  <span v-if="!hasCrew">{{ $t("profile.activeFlag.status.noCrew") }}</span>
-		  <a class="disabled vca-button-primary vca-full-width" >{{ $t("profile.activeFlag.actions.cannotApply") }}</a>
-		</div>
-	</div>
+                <div class="active-flag-status" v-if="canApply()">
+                  <span>{{ $t("profile.activeFlag.status.apply") }}</span>
+                  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleActiveRequest">{{ $t("profile.activeFlag.actions.apply") }}</a>
+                </div>
+
+                <div class="active-flag-status" v-if="isDenied()">
+                  <span><strong>{{ $t("profile.warning.important") }}</strong></span>
+                  <span v-if="!hasCrew">{{ $t("profile.activeFlag.status.noCrew") }}</span>
+                  <a class="disabled vca-button-primary vca-full-width" >{{ $t("profile.activeFlag.actions.cannotApply") }}</a>
+                </div>
+        </div>
 </template>
 
 <script>
@@ -35,7 +42,7 @@
     import axios from 'axios'
     import {
         Notification,
-	Button
+        Button
     } from 'element-ui'
     Vue.use(Notification);
     Vue.use(Button);
@@ -45,55 +52,61 @@
         name: "ActiveFlag",
         data() {
             return {
-		hasCrew: false,
-		isActive: false,
-		isRequested: false
+                hasCrew: false,
+                isActive: false,
+                isRequested: false
             }
         },
         created() {
             this.init()
         },
-	mounted() {
-		this.$root.$on('activeState', () => { this.init(); })
-	},
+        mounted() {
+                this.$root.$on('activeState', () => { this.init(); })
+        },
         methods: {
-	    canApply() { return (!this.isActive && this.hasCrew && !this.isRequested); },
-	    isDenied() { return (!this.hasCrew && !this.isActive); },
+            canApply() { return (!this.isActive && this.hasCrew && !this.isRequested); },
+            isDenied() { return (!this.hasCrew && !this.isActive); },
+            /* Set the active state of the supporter to inactive */
             handleSetInactiveRequest(event) {
-		if (!confirm(this.$t('profile.activeFlag.messages.inactive.confirm'))) {
-			return false;
-		}
-		this.submit('/drops/webapp/profile/active/inactive');
-	    },
-            handleUnsetActiveRequest(event) {
-		if (!confirm(this.$t('profile.activeFlag.messages.unset.confirm'))) {
-			return false;
-		}
-		this.submit('/drops/webapp/profile/active/inactive');
-	    },
-            handleActiveRequest(event) {
-		if (!confirm(this.$t('profile.activeFlag.messages.request.confirm'))) {
-			return false;
-		}
-		this.submit('/drops/webapp/profile/active/request')
+                if (!confirm(this.$t('profile.activeFlag.messages.inactive.confirm'))) {
+                        return false;
+                }
+                this.submit('/drops/webapp/profile/active/inactive');
             },
+            /* Set the active requested state of the supporter back to inactive */
+            handleUnsetActiveRequest(event) {
+                if (!confirm(this.$t('profile.activeFlag.messages.unset.confirm'))) {
+                        return false;
+                }
+                this.submit('/drops/webapp/profile/active/inactive');
+            },
+            /* Set the active state of the supporter to requested to apply for active state */
+            handleActiveRequest(event) {
+                if (!confirm(this.$t('profile.activeFlag.messages.request.confirm'))) {
+                        return false;
+                }
+                this.submit('/drops/webapp/profile/active/request')
+            },
+            /* Send the request to the backend to change current state */
             submit(url) {
-                
-        	this.axios
-        	  .post(url)
-       	 	  .then(response => {
-            		switch (response.status) {
-              			case 200:
-                			this.open(
-                  				this.$t('profile.activeFlag.messages.request.success.title'),
-                  				this.$t('profile.activeFlag.messages.request.success.message'),
-                  				"success"
-                			)
-					this.setState(response.data.additional_information.status);
-					this.$root.$emit('nvmState');
-                		break;
-            		}
-        	}).catch(error => {
+                this.axios
+                  .post(url)
+                          .then(response => {
+                            switch (response.status) {
+                                // TODO: Handle other return codes
+                                      case 200:
+                                        // TODO: Add return state to the message to show a more specific message
+                                        this.open(
+                                                  this.$t('profile.activeFlag.messages.request.success.title'),
+                                                  this.$t('profile.activeFlag.messages.request.success.message'),
+                                                  "success"
+                                        )
+                                        this.setState(response.data.additional_information.status);
+                                        // Emit nvmState to check if the non-voting-membership state is affected by the change of the active state
+                                        this.$root.$emit('nvmState');
+                                break;
+                            }
+                }).catch(error => {
                     this.selection = this.default
                     this.open(
                         this.$t('profile.activeFlag.notification.error.title'),
@@ -102,34 +115,34 @@
                     )
                 })
             },
-	    setState(status) {
-			switch(status) {
-				case 'active':
-					this.isActive = true;
-					this.isRequested = false;
-					break;
-				case 'requested':
-					this.isActive = false;
-					this.isRequested = true;
-					break;
-				case 'inactive':
-				default:
-					this.isActive = false;
-					this.isRequested = false;
-					break;
-			}
-	    },
+            // Set the state 
+            setState(status) {
+                        switch(status) {
+                                case 'active':
+                                        this.isActive = true;
+                                        this.isRequested = false;
+                                        break;
+                                case 'requested':
+                                        this.isActive = false;
+                                        this.isRequested = true;
+                                        break;
+                                case 'inactive':
+                                default:
+                                        this.isActive = false;
+                                        this.isRequested = false;
+                                        break;
+                        }
+            },
             init() {
                 this.axios
-		  .get('/drops/webapp/profile/active/check')
-		  .then(response => {
-		    switch (response.status) {
-		      case 200:
-			console.log(response.data.additional_information);
-			this.hasCrew = (response.data.additional_information.conditions.hasPrimaryCrew);
-			this.setState(response.data.additional_information.status)
-		    }
-		}).catch(error => {
+                  .get('/drops/webapp/profile/active/check')
+                  .then(response => {
+                    switch (response.status) {
+                      case 200:
+                        this.hasCrew = (response.data.additional_information.conditions.hasPrimaryCrew);
+                        this.setState(response.data.additional_information.status)
+                    }
+                }).catch(error => {
                     this.selection = this.default
                 })
             },
--- src/components/NonVotingMembership.vue
+++ src/components/NonVotingMembership.vue
@@ -1,37 +1,41 @@
 <template>
-    	<div class="non-voting-membership">
-		<div class="nvmTitle"><strong class="nvmTitle-title">{{ $t("profile.nvm.title") }}</strong>
-			<span class="nvm-active" v-if="isNVM">{{ $t("profile.nvm.current.active") }}</span>
-			<span class="nvm-expired" v-if="isExpired">{{ $t("profile.nvm.current.expired") }}</span>
-			<span class="nvm-inactive" v-if="canApply">{{ $t("profile.nvm.current.inactive") }}</span>
-			<span class="nvm-denied" v-if="isDenied()">{{ $t("profile.nvm.current.denied") }}</span>
-		</div>
-		<div class="nvmDescription">{{ $t("profile.nvm.description") }}</div>
-		
-		<div class="nvm" v-if="isNVM">
-		  <span><strong>{{ $t("profile.nvm.status.active") }}</strong></span>
-		  <a class="vca-button-warn vca-full-width" style="cursor: pointer;" @click.prevent="handleSetInactiveRequest">{{ $t("profile.nvm.actions.active") }}</a>
-		</div>
+            <div class="non-voting-membership">
+                <div class="nvmTitle"><strong class="nvmTitle-title">{{ $t("profile.nvm.title") }}</strong>
+                        <span class="nvm-active" v-if="isNVM">{{ $t("profile.nvm.current.active") }}</span>
+                        <span class="nvm-expired" v-if="isExpired">{{ $t("profile.nvm.current.expired") }}</span>
+                        <span class="nvm-inactive" v-if="canApply">{{ $t("profile.nvm.current.inactive") }}</span>
+                        <span class="nvm-denied" v-if="isDenied()">{{ $t("profile.nvm.current.denied") }}</span>
+                </div>
+                <div class="nvmDescription">{{ $t("profile.nvm.description") }}</div>
 
-		<div class="nvm" v-if="isExpired">
-		  <span>{{ $t("profile.nvm.status.renew") }}</span>
-		  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleActiveRequest">{{ $t("profile.nvm.actions.renew") }}</a>
-		</div>
+                <el-card class="box-card tail expand spaced">
+                    <a :href="articleOfAssociationLink" target="_blank">{{ $t("profile.nvm.aoa") }}</a>
+                </el-card>
 
-		<div class="nvm" v-if="canApply">
-		  <span>{{ $t("profile.nvm.status.apply") }}</span><br/><br/>
-		  <span>{{ $t("profile.nvm.status.confirmation") }}</span>
-		  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleActiveRequest">{{ $t("profile.nvm.actions.apply") }}</a>
-		</div>
+                <div class="nvm" v-if="isNVM">
+                  <span><strong>{{ $t("profile.nvm.status.active") }}</strong></span>
+                  <a class="vca-button-warn vca-full-width" style="cursor: pointer;" @click.prevent="handleSetInactiveRequest">{{ $t("profile.nvm.actions.active") }}</a>
+                </div>
 
-		<div class="nvm" v-if="isDenied()">
-		  <span><strong>{{ $t("profile.warning.important") }}</strong></span>
-		  <span v-if="!hasAddress">{{ $t("profile.nvm.status.noAddress") }}</span>
-		  <span v-if="!isActive">{{ $t("profile.nvm.status.inActive") }}</span>
-		  <span v-if="!hasPrimaryCrew">{{ $t("profile.nvm.status.noPrimaryCrew") }}</span>
-		  <a class="disabled vca-button-primary vca-full-width" >{{ $t("profile.nvm.actions.cannotApply") }}</a>
-		</div>
-	</div>
+                <div class="nvm" v-if="isExpired">
+                  <span>{{ $t("profile.nvm.status.renew") }}</span>
+                  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleActiveRequest">{{ $t("profile.nvm.actions.renew") }}</a>
+                </div>
+
+                <div class="nvm" v-if="canApply">
+                  <span>{{ $t("profile.nvm.status.apply") }}</span><br/><br/>
+                  <span>{{ $t("profile.nvm.status.confirmation") }}</span>
+                  <a class="vca-button-primary vca-full-width" style="cursor: pointer;" @click.prevent="handleActiveRequest">{{ $t("profile.nvm.actions.apply") }}</a>
+                </div>
+
+                <div class="nvm" v-if="isDenied()">
+                  <span><strong>{{ $t("profile.warning.important") }}</strong></span>
+                  <span v-if="!hasAddress">{{ $t("profile.nvm.status.noAddress") }}</span>
+                  <span v-if="!isActive">{{ $t("profile.nvm.status.inActive") }}</span>
+                  <span v-if="!hasPrimaryCrew">{{ $t("profile.nvm.status.noPrimaryCrew") }}</span>
+                  <a class="disabled vca-button-primary vca-full-width" >{{ $t("profile.nvm.actions.cannotApply") }}</a>
+                </div>
+        </div>
 </template>
 
 <script>
@@ -39,7 +43,7 @@
     import axios from 'axios'
     import {
         Notification,
-	Button
+        Button
     } from 'element-ui'
     Vue.use(Notification);
     Vue.use(Button);
@@ -49,50 +53,51 @@
         name: "NonVotingMembership",
         data() {
             return {
-		hasAddress: true,
-		isActive: true,
-		canApply: false,
-		hasPrimaryCrew: true,
-		isExpired: false,
-		isNVM: false
+                articleOfAssociationLink: this.$t('profile.nvm.link'),
+                hasAddress: true,
+                isActive: true,
+                canApply: false,
+                hasPrimaryCrew: true,
+                isExpired: false,
+                isNVM: false
             }
         },
         created() {
             this.init()
         },
-	mounted() {
-		this.$root.$on('nvmState', () => { this.init(); })
-	},
+        mounted() {
+                this.$root.$on('nvmState', () => { this.init(); })
+        },
         methods: {
-	    isDenied() { return (!this.canApply && !this.isExpired && !this.isNVM); },
-	    handleSetInactiveRequest(event) {
-		if (!confirm(this.$t('profile.nvm.messages.inactive.confirm'))) {
-			return false;
-		}
-		this.submit('/drops/webapp/profile/nvm/inactive');
-	    },
+            isDenied() { return (!this.canApply && !this.isExpired && !this.isNVM); },
+            handleSetInactiveRequest(event) {
+                if (!confirm(this.$t('profile.nvm.messages.inactive.confirm'))) {
+                        return false;
+                }
+                this.submit('/drops/webapp/profile/nvm/inactive');
+            },
             handleActiveRequest(event) {
-		if (!confirm(this.$t('profile.nvm.messages.request.confirm'))) {
-			return false;
-		}
-		this.submit('/drops/webapp/profile/nvm/request')
+                if (!confirm(this.$t('profile.nvm.messages.request.confirm'))) {
+                        return false;
+                }
+                this.submit('/drops/webapp/profile/nvm/request')
             },
             submit(url) {
                 
-        	this.axios
-        	  .post(url)
-       	 	  .then(response => {
-            		switch (response.status) {
-              			case 200:
-                			this.open(
-                  				this.$t('profile.nvm.messages.request.success.title'),
-                  				this.$t('profile.nvm.messages.request.success.message'),
-                  				"success"
-                			)
-					this.init();
-                		break;
-            		}
-        	}).catch(error => {
+                this.axios
+                  .post(url)
+                          .then(response => {
+                            switch (response.status) {
+                                      case 200:
+                                        this.open(
+                                                  this.$t('profile.nvm.messages.request.success.title'),
+                                                  this.$t('profile.nvm.messages.request.success.message'),
+                                                  "success"
+                                        )
+                                        this.init();
+                                break;
+                            }
+                }).catch(error => {
                     this.selection = this.default
                     this.open(
                         this.$t('profile.nvm.notification.error.title'),
@@ -103,38 +108,38 @@
             },
             init() {
                 this.axios
-		  .get('/drops/webapp/profile/nvm/check')
-		  .then(response => {
-		    switch (response.status) {
-		      case 200:
-			switch(response.data.additional_information.status) {
-				case 'expired':
-					this.isExpired = true;
-					this.isNVM = false;
-					this.canApply = false;
-					break;
-				case 'active':
-					this.isNVM = true;
-					this.isExpired = false;
-					this.canApply = false;
-					break;
-				case 'inactive':
-					this.canApply = true;
-					this.isExpired = false;
-					this.isNVM = false;
-					break;
-				case 'denied':
-				default:
-					this.isNVM = false;
-					this.isExpired = false;
-					this.canApply = false;
-					this.hasAddress = (response.data.additional_information.conditions.hasAddress);
-					this.isActive = (response.data.additional_information.conditions.isActive);
-					this.hasPrimaryCrew = (response.data.additional_information.conditions.hasPrimaryCrew);
-					break;
-			}
-		    }
-		}).catch(error => {
+                  .get('/drops/webapp/profile/nvm/check')
+                  .then(response => {
+                    switch (response.status) {
+                      case 200:
+                        switch(response.data.additional_information.status) {
+                                case 'expired':
+                                        this.isExpired = true;
+                                        this.isNVM = false;
+                                        this.canApply = false;
+                                        break;
+                                case 'active':
+                                        this.isNVM = true;
+                                        this.isExpired = false;
+                                        this.canApply = false;
+                                        break;
+                                case 'inactive':
+                                        this.canApply = true;
+                                        this.isExpired = false;
+                                        this.isNVM = false;
+                                        break;
+                                case 'denied':
+                                default:
+                                        this.isNVM = false;
+                                        this.isExpired = false;
+                                        this.canApply = false;
+                                        this.hasAddress = (response.data.additional_information.conditions.hasAddress);
+                                        this.isActive = (response.data.additional_information.conditions.isActive);
+                                        this.hasPrimaryCrew = (response.data.additional_information.conditions.hasPrimaryCrew);
+                                        break;
+                        }
+                    }
+                }).catch(error => {
                     this.selection = this.default
                 })
             },
@@ -150,7 +155,7 @@
 </script>
 
 <style scoped>
-
+.spaced { margin: 10px 0; }
 .nvmTitle-title { margin-right: 5px; }
 .nvm-active { color: green; }
 .nvm-inactive { color: grey; }
